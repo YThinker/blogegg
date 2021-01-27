9b92aba02dd4ddbbeef3f9df45e68b44c2ffc095
xingkaiyang <xingkai.yang@loongair.cn>
2021-01-27 22:01:02 +0800

    删除注释

--- config/config.default.js
+++ config/config.default.js
@@ -14,15 +14,10 @@ module.exports = appInfo => {
   config.mysql = {
     // 单数据库信息配置
     client: {
-      // host
       host: 'localhost',
-      // 端口号
       port: '3306',
-      // 用户名
       user: 'root',
-      // 密码
       password: 'yxk980102',
-      // 数据库名
       database: 'eggblog',
     },
     // 是否加载到 app 上，默认开启
